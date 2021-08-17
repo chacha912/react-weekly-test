c245d3753dd4c5ebec2fb5de2b5560e8e013cdf9
chacha912 <dkwkdnwk912@gmail.com>
2021-08-18 00:33:43 +0900

    feat: Markup and Style FaqList & FaqListItem

---
+++ src/components/FaqListItem/FaqListItem.jsx
@@ -0,0 +1,43 @@
+import styles from './FaqListItem.module.scss';
+import { Component, Fragment } from 'react';
+import { classNames } from 'utils';
+import { ReactComponent as SvgIconPlus } from 'assets/icons/plus.svg';
+import { ReactComponent as SvgIconClose } from 'assets/icons/close.svg';
+
+export class FaqListItem extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const {
+      faqData: { title, answer },
+    } = this.props;
+    return (
+      <li className={classNames(styles.list, isOpen ? styles.open : '')}>
+        <button className={styles.handleButton}>
+          <span className={styles.title}>{title}</span>
+          {isOpen ? (
+            <SvgIconClose className={styles.icon} />
+          ) : (
+            <SvgIconPlus className={styles.icon} />
+          )}
+        </button>
+        <p className={styles.answer}>
+          {answer.map((content, index, { length }) => (
+            <Fragment key={`answer-${index}`}>
+              {content}
+              {index < length - 1 && (
+                <>
+                  <br />
+                  <br />
+                </>
+              )}
+            </Fragment>
+          ))}
+        </p>
+      </li>
+    );
+  }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './A11yHidden/A11yHidden';
 export * from './Heading/Heading';
+export * from './FaqListItem/FaqListItem';
